9cac2abcfcfa3df2c70284aee1bd979fb208025d
LimTangmeng <limtangmeng100@gmail.com>
2024-06-17 17:46:33 +0700

    feat: add extension gif to mp4 convertor

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.contextMenus.create({
+  id: "gif-to-mp4",
+  title: "GIF to MP4",
+  contexts: ["image"],
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  console.log(info);
+  if (info.menuItemId == "gif-to-mp4") {
+    var gif_url = info.srcUrl.replace(".webp", ".gif");
+    chrome.tabs.create({
+      url: `https://ezgif.com/video-to-gif?url=${gif_url}`,
+    });
+  }
+});
